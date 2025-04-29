c3d45947e8c7f2f7bd7f19d76c9abfb8d753c135
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-04-29 10:56:20 +0900

    feat: origin

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL.split(','),
     credentials: true,
   });
 
